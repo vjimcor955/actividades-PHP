b47422887c045fd326b4f584bc4d91026afb1d5b
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-30 15:19:04 +0100

    index

---
+++ codigophp/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página principal</title>
+</head>
+<body>
+    <a href="phpinfo.php">phpinfo()</a>,
+    <a href="xdebug_info.php">xdebug_info()</a>
+    <br>
+    <h1>Ejercicios PHP:</h1>
+    <ul>
+        <h4>1.- Variables y constantes:</h4>
+        <li><a href="1-variables/ejercicio_1.php">Ejercicio 1</a></li>
+        <li><a href="1-variables/ejercicio_2.php">Ejercicio 2</a></li>
+        <li><a href="1-variables/ejercicio_3.php">Ejercicio 3</a></li>
+        <li><a href="1-variables/ejercicio_4.php">Ejercicio 4</a></li>
+    </ul>
+    <ul>
+        <h4>2.- Formularios</h4>
+        <li><a href="2-formularios/ejercicio_1.php">Ejercicio 1</a> <br>
+        <li><a href="2-formularios/ejercicio_2.php">Ejercicio 2</a> <br>
+        <li><a href="2-formularios/ejercicio_3.php">Ejercicio 3</a> <br>
+        <li><a href="2-formularios/ejercicio_4.php">Ejercicio 4</a> <br>
+    </ul>
+    <ul>
+        <h4>3.- Estructuras de control</h4>
+        <li><a href="3-estructuras-de-control/ejercicio_1.php">Ejercicio 1</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_2.php">Ejercicio 2</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_3.php">Ejercicio 3</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_4.php">Ejercicio 4</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_5.php">Ejercicio 5</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_6.php">Ejercicio 6</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_7.php">Ejercicio 7</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_8.php">Ejercicio 8</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_9.php">Ejercicio 9</a></li>
+        <li><a href="3-estructuras-de-control/ejercicio_10.php">Ejercicio 10</a></li>
+    </ul>
+    <ul>
+        <h4>4.- Includes y páginas reiteradas</h4>
+        <li><a href="4-includes-y-paginas-reiteradas/ejercicio_1.php">Ejercicio 1</a></li>
+        <li><a href="4-includes-y-paginas-reiteradas/ejercicio_2.php">Ejercicio 2</a></li>
+        <li><a href="4-includes-y-paginas-reiteradas/ejercicio_3.php">Ejercicio 3</a></li>
+    </ul>
+    <ul>
+        <h4>5.- Funciones</h4>
+        <li><a href="5-funciones/biblioteca.php">Biblioteca</a></li>
+    </ul>
+    <ul>
+        <h4>6.- Arrays</h4>
+        <li><a href="6-arrays/ejercicio-1.php">Ejercicio 1</a></li>
+    </ul>
+</body>
+</html>
